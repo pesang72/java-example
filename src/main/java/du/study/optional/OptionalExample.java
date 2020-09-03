5133307b0f807fbf497dfa22377599f87d732a28
Parkdusang <pdsnm486@hanmail.net>
2020-09-03 21:57:48 +0900

    project setting

---
+++ src/main/java/du/study/optional/OptionalExample.java
@@ -0,0 +1,11 @@
+package du.study.optional;
+
+
+
+public class OptionalExample {
+
+    public static void main(String[] args) {
+
+        List<String> list =
+    }
+}
---
+++ src/main/java/du/study/stream/StreamExample.java
@@ -0,0 +1,13 @@
+package du.study.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class StreamExample {
+
+    public static void main(String[] args) {
+        List<String> list = Arrays.asList(new String[]{"test1", "test2", "test3"});
+
+        
+    }
+}
