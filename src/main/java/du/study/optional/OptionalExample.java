31405f10f62ccd8aa9135e5df4bf7ca596cdd328
Parkdusang <pdsnm486@hanmail.net>
2020-09-03 22:22:01 +0900

    add optional example

--- src/main/java/du/study/optional/OptionalExample.java
+++ src/main/java/du/study/optional/OptionalExample.java
@@ -1,11 +1,71 @@
 package du.study.optional;
 
-
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 public class OptionalExample {
 
     public static void main(String[] args) {
-
-        List<String> list =
+        new OptionalExample();
     }
+
+    public OptionalExample(){
+        init();
+    }
+
+    private void init() {
+        // 1. orElse example
+        orElseTest();
+
+        // 2. orElseGet example
+        orElseGetTest();
+
+        // 3. orElseThrow example
+        orElseThrowTest();
+
+        // 4. isPresent example
+        isPresentTest();
+
+    }
+
+    private static Optional<String> optionalNullTest(){
+        return Optional.ofNullable(null);
+    }
+
+    private static Optional<String> optionalValueTest(){
+        return Optional.ofNullable("hello world");
+    }
+
+    private void orElseTest(){
+        String orElse = optionalNullTest().orElse("orElse");
+        System.out.println(orElse); // orElse
+
+        orElse = optionalValueTest().orElse("else case");
+        System.out.println(orElse); // hello world
+    }
+
+    private void orElseGetTest(){
+        String orElseGet = optionalNullTest().orElseGet(() -> "orElseGet");
+        System.out.println(orElseGet); // orElseGet
+
+        orElseGet = optionalValueTest().orElseGet(() -> "orElseGet");
+        System.out.println(orElseGet); // hello world
+    }
+
+    private void orElseThrowTest(){
+        String orElseThrow = optionalNullTest().orElseThrow(() -> new NoSuchElementException());
+        System.out.println(orElseThrow); // Exception in thread "main" java.util.NoSuchElementException
+
+        orElseThrow = optionalValueTest().orElseThrow(() -> new NoSuchElementException());
+        System.out.println(orElseThrow); //  hello world
+    }
+
+    private void isPresentTest(){
+        Optional<String> optionalTest = optionalValueTest();
+        if(optionalTest.isPresent()){
+            String test = optionalTest.get();
+            System.out.println(test); // hello world
+        }
+    }
+
 }
